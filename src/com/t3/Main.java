8c6e2e5d1285bc1dd15a4969e4822f405c8564d5
Alex <alexander.macey@idealinnovations.com>
2019-06-28 17:21:41 -0400

    It works! Has a generational algorithm for playing tictactoe against a Chaos player who just moves randomly. Current algorithm can win ~90% of the time.

--- src/com/t3/Main.java
+++ src/com/t3/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Beginning Simulation...");
+        game.start();
+        System.out.println("Simulation complete.");
     }
 }
